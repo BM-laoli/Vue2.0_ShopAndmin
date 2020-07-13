cabda6055d3aaeb4cb0d971c60ea97b5efbdb17e
cuz <285240216@qq.com>
2020-07-13 20:14:45 +0800

    over

--- src/mock/index.js
+++ src/mock/index.js
@@ -2,14 +2,19 @@
 import Mock from 'mockjs'
 // 引入模板函数类
 import ratings from './modules/ratings'
+import cuz from './modules/cuz'
 
 // Mock函数
 const { mock } = Mock
 
 // 设置延时
 Mock.setup({
-  timeout: 400
+    timeout: 400,
 })
 
 // 使用拦截规则拦截命中的请求，mock(url, post/get, 返回的数据);
 mock(/\/api\/ratings\/list/, 'post', ratings.list)
+
+// 推广列表请求
+mock(/\/api\/cuz\/userList/, 'get', cuz.userList)
+mock(/\/api\/cuz\/shopList/, 'get', cuz.shopList)
