dbe445e5548253e555343aaa9297d133d01e2901
bmlaoli <861795660@qq.com>
2020-07-14 19:36:19 +0800

    商圈管理基础渲染完成

---
+++ src/components/business-district/personal-hader-from.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+      <el-form :inline="true"  label-width="150px" :model="formInline" class="demo-form-inline">
+        <el-form-item label="用户ID/昵称">
+          <el-input v-model="formInline.query" placeholder="请输入用户ID/昵称"></el-input>
+        </el-form-item>
+
+        <el-form-item label="日期">
+          <el-date-picker
+            v-model="value"
+            type="daterange"
+            :picker-options="pickerOptions"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+            align="right">
+          </el-date-picker>
+        </el-form-item>
+       
+
+        <el-form-item>  
+           <div class="comfrim">
+            <el-button type="primary" @click="onSubmit">查询</el-button>
+            <el-button type="primary" @click="onCancel">重置</el-button>
+          </div>
+        </el-form-item>
+      </el-form>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'HaderForm',
+    data() {
+      return {
+        formInline: {
+          query:'',
+        },
+         pickerOptions: {
+          shortcuts: [{
+            text: '最近一周',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近一个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+              picker.$emit('pick', [start, end]);
+            }
+          }, {
+            text: '最近三个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+              picker.$emit('pick', [start, end]);
+            }
+          }]
+        },
+        value: ''
+
+      }
+    },
+    methods: {
+      onSubmit() {
+        
+      },
+      onCancel(){}
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+ .el-form {
+    padding-top: 20px;
+  }
+.comfrim {
+    margin-left: 150px;
+    padding-right: 15px;
+  }
+</style>
--- src/views/setting/parms.vue
+++ src/views/setting/parms.vue
@@ -1,55 +1,55 @@
 <template>
   <div>
-    <bread-crumbs :level="this.$route.meta"></bread-crumbs>
+   <bread-crumbs :level="this.$route.meta"></bread-crumbs>
     <el-card class="box-card">
-      <span>店铺订单抽佣比例</span>
-      <div class="box">
-        <span>抽佣比</span>
-        <input type="text" />
-        <span>%</span>
+      <!-- heder头部 -->
+      <div class="hedaer-box">
+          
       </div>
-    </el-card>
-    <el-card class="box-card">
-      <span>用户素材收佣比例</span>
-      <div class="box">
-        <span>抽佣比</span>
-        <input type="text" />
-        <span>%</span>
+
+      <div class="border-middleware">
+          
       </div>
+      <!-- body 身体 -->
+      <div class="tabbe-box">
+        
+      </div>
+      
     </el-card>
-    <el-button type="primary" class="saveBtn">保存</el-button>
   </div>
 </template>
 
 <script>
-import breadCrumbs from "../../components/common/bread-crumbs";
-export default {
-  components: {
-    breadCrumbs
+import breadCrumbs from '../../components/common/bread-crumbs'
+  export default {
+    components: {
+      breadCrumbs,
+    },  
   }
-};
 </script>
 
 <style lang="scss" scoped>
 .el-card {
   margin: 15px;
   margin-top: 22px;
-  background-color: #e6e6fa;
-  width: 60%;
-  border-radius: 10px;
-  .box {
-    margin-left: 50px;
-    margin-top: 20px;
-
-    input {
-      width: 30px;
-      margin: 0 10px;
-    }
-  }
+  background-color:	#E6E6FA;
+  padding: 8px;
 }
-.saveBtn {
-  width: 150px;
-  height: 40px;
-  margin: 20px 0 0 60px;
+.hedaer-box {
+  width: 100%;
+  height: 150px;
+  background-color: pink;
+}
+.tabbe-box {
+  width: 100%;
+  height: 1000px;
+  background-color: cornflowerblue;
+}
+
+.border-middleware {
+  width: 100%;
+  height: 80px;
+  background-color: yellowgreen;
+  margin: 10px 0;
 }
 </style>
