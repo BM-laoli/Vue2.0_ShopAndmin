dfbd5d81ccd83117e6cc66ac12b188c13859f9cb
Queenie <cjqueenie@163.com>
2020-07-18 10:10:34 +0800

    样式统一和地图图标自定义和放大完毕

--- src/components/baiduMap/baidu-map.vue
+++ src/components/baiduMap/baidu-map.vue
@@ -5,7 +5,6 @@
       :zoom="zoom"
       @ready="handler"
       style="height:400px; width:600px"
-      @click="getClickInfo"
       :scroll-wheel-zoom="true"
       :double-click-zoom="false"
     >
@@ -15,7 +14,6 @@
       disabled
       style="width:250px; display: inline-block; margin-right:50px; margin-top:20px"
       v-model="preAddress"
-      @on-blur="getMapPoint"
     ></el-input>
     <el-input
       disabled
@@ -42,7 +40,7 @@ export default {
   data() {
     return {
       center: {},
-      zoom: 13,
+      zoom: 20,
       preAddress: "",
       detailAddress: ""
     };
@@ -55,102 +53,102 @@ export default {
   },
   mounted() {},
   methods: {
-    // 当以及地址input失焦时触发在页面上显示点的事件
-    getMapPoint() {
-      // console.log("this.address", this.address);
-      const geocoder = new BMap.Geocoder();
-      geocoder.getPoint(this.preAddress, function(res) {
-        console.log(res);
-        const { lng, lat } = res;
-        var point = new BMap.Point(lng, lat); // 用于描述地图上点的经纬度。
-        map.centerAndZoom(point, 13);
-        var marker = new BMap.Marker(point); // 创建标注
-        console.log("marker----", marker);
-
-        map.addOverlay(marker); // 将标注添加到地图中
-        var circle = new BMap.Circle(point, 6, {
-          strokeColor: "Red",
-          strokeWeight: 6,
-          strokeOpacity: 1,
-          Color: "Red",
-          fillColor: "#f03"
-        });
-        map.addOverlay(circle);
-      });
-    },
+    // 地图一开始渲染时触发
     handler({ BMap, map }) {
-      // console.log("this.address", this.address);
-      // console.log("this.preAddress", this.preAddress);
       const geocoder = new BMap.Geocoder();
       geocoder.getPoint(this.preAddress + this.detailAddress, function(res) {
         console.log(res);
         const { lng, lat } = res;
-        var point = new BMap.Point(lng, lat); // 用于描述地图上点的经纬度。
-        map.centerAndZoom(point, 13);
-        var marker = new BMap.Marker(point); // 创建标注
-        console.log("marker----", marker);
-
-        map.addOverlay(marker); // 将标注添加到地图中
-        var circle = new BMap.Circle(point, 6, {
-          strokeColor: "Red",
-          strokeWeight: 6,
-          strokeOpacity: 1,
-          Color: "Red",
-          fillColor: "#f03"
+        // 用于描述地图上点的经纬度。
+        var point = new BMap.Point(lng, lat);
+        console.log("point", point);
+        map.centerAndZoom(point, 15);
+        var myIcon = new BMap.Icon(
+          require("../../assets/baidu.png"),
+          new BMap.Size(60, 60)
+        );
+        var marker = new BMap.Marker(point, {
+          icon: myIcon
         });
-        map.addOverlay(circle);
+        map.addOverlay(marker);
+        // var circle = new BMap.Circle(point, 6, {
+        //   strokeColor: "Red",
+        //   strokeWeight: 6,
+        //   strokeOpacity: 1,
+        //   Color: "Red",
+        //   fillColor: "#f03"
+        // });
+        // map.addOverlay(circle);
+        // 这是为了将map挂载到全局，方便后续调用
         window.map = map;
       });
-    },
-    getClickInfo(e) {
-      map.clearOverlays();
-      this.center.lng = e.point.lng;
-      this.center.lat = e.point.lat;
-      var point = new BMap.Point(this.center.lng, this.center.lat); // 用于描述地图上点的经纬度。
-      var marker = new BMap.Marker(point); // 创建标注
-      console.log("marker----", marker);
-
-      map.addOverlay(marker); // 将标注添加到地图中
-      var circle = new BMap.Circle(point, 6, {
-        strokeColor: "Red",
-        strokeWeight: 6,
-        strokeOpacity: 1,
-        Color: "Red",
-        fillColor: "#f03"
-      });
-      map.addOverlay(circle);
-      var geocoder = new BMap.Geocoder();
-      geocoder.getLocation(e.point, rs => {
-        console.log("地址描述(string)", rs.address); // 地址描述(string)
-
-        console.log("结构化的地址描述(object)", rs.addressComponents); // 结构化的地址描述(object)
-
-        this.preAddress =
-          rs.addressComponents.province +
-          rs.addressComponents.city +
-          rs.addressComponents.district;
-        this.detailAddress =
-          rs.addressComponents.street + rs.addressComponents.streetNumber;
-        console.log("省", rs.addressComponents.province); // 省
-        console.log("城市", rs.addressComponents.city); // 城市
-        console.log("区县", rs.addressComponents.district); // 区县
-        console.log("街道", rs.addressComponents.street); // 街道
-        console.log("门牌号", rs.addressComponents.streetNumber); // 门牌号
-
-        console.log("附近的POI点(array)", rs.surroundingPois); // 附近的POI点(array)
-
-        console.log("商圈字段，代表此点所属的商圈(string)", rs.business); // 商圈字段，代表此点所属的商圈(string)
-      });
-    },
-    handleSubmit(name) {
-      this.$refs[name].validate(valid => {
-        if (valid) {
-          this.$Message.success("Success!");
-        } else {
-          this.$Message.error("Fail!");
-        }
-      });
     }
+    // 当第一个地址input失焦时触发，在页面上显示该地址对于的点
+    // getMapPoint() {
+    //   // console.log("this.address", this.address);
+    //   const geocoder = new BMap.Geocoder();
+    //   geocoder.getPoint(this.preAddress, function(res) {
+    //     console.log(res);
+    //     const { lng, lat } = res;
+    //     var point = new BMap.Point(lng, lat); // 用于描述地图上点的经纬度。
+    //     map.centerAndZoom(point, 13);
+    //     var marker = new BMap.Marker(point); // 创建标注
+    //     console.log("marker----", marker);
+
+    //     map.addOverlay(marker); // 将标注添加到地图中
+    //     var circle = new BMap.Circle(point, 6, {
+    //       strokeColor: "Red",
+    //       strokeWeight: 6,
+    //       strokeOpacity: 1,
+    //       Color: "Red",
+    //       fillColor: "#f03"
+    //     });
+    //     map.addOverlay(circle);
+    //   });
+    // },
+
+    // 点击地图上到位置可在input框中显示具体的中文地址
+    // getClickInfo(e) {
+    //   console.log("e.point", e.point);
+    //   map.clearOverlays();
+    //   this.center.lng = e.point.lng;
+    //   this.center.lat = e.point.lat;
+    //   var point = new BMap.Point(this.center.lng, this.center.lat); // 用于描述地图上点的经纬度。
+    //   var marker = new BMap.Marker(point); // 创建标注
+    //   console.log("marker----", marker);
+
+    //   map.addOverlay(marker); // 将标注添加到地图中
+    //   var circle = new BMap.Circle(point, 1, {
+    //     strokeColor: "Red",
+    //     strokeWeight: 6,
+    //     strokeOpacity: 1,
+    //     Color: "Red",
+    //     fillColor: "#f03"
+    //   });
+    //   map.addOverlay(circle);
+    //   var geocoder = new BMap.Geocoder();
+    //   geocoder.getLocation(e.point, rs => {
+    //     console.log("地址描述(string)", rs.address); // 地址描述(string)
+
+    //     console.log("结构化的地址描述(object)", rs.addressComponents); // 结构化的地址描述(object)
+
+    //     this.preAddress =
+    //       rs.addressComponents.province +
+    //       rs.addressComponents.city +
+    //       rs.addressComponents.district;
+    //     this.detailAddress =
+    //       rs.addressComponents.street + rs.addressComponents.streetNumber;
+    //     console.log("省", rs.addressComponents.province); // 省
+    //     console.log("城市", rs.addressComponents.city); // 城市
+    //     console.log("区县", rs.addressComponents.district); // 区县
+    //     console.log("街道", rs.addressComponents.street); // 街道
+    //     console.log("门牌号", rs.addressComponents.streetNumber); // 门牌号
+
+    //     console.log("附近的POI点(array)", rs.surroundingPois); // 附近的POI点(array)
+
+    //     console.log("商圈字段，代表此点所属的商圈(string)", rs.business); // 商圈字段，代表此点所属的商圈(string)
+    //   });
+    // }
   }
 };
 </script>
